837b96f1ed35f26786933a9456016b440395629d
justPoosay <asustufgamingdesktop@gmail.com>
2024-12-22 21:54:58 +0100

    Przykladowy layout i frontend - oscar

---
+++ src/Product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    name: string;
+    calories: number;
+    category: string;
+    favorite: boolean;
+  }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import AddProductForm from "../components/AddProductForm";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Strona glowna</h1>
+      <AddProductForm />
+    </div>
+  );
+};
+
+export default Home;
