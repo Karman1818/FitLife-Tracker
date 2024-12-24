accf871b902685171f90938c34f6b483f59798f2
Karman <u38_markar_lbn@technischools.com>
2024-12-25 00:03:25 +0100

    test

--- src/state/productStore.ts
+++ src/state/productStore.ts
@@ -26,6 +26,7 @@ export const useProductStore = create((set) => ({
     addProduct: (product) =>
         set((state) => ({
             products: [...state.products, product],
+            // test
         }))
 
 
