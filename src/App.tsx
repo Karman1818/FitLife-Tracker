3be000a5075875cc557262544dd3b1374d793c48
Karman <u38_markar_lbn@technischools.com>
2025-01-04 16:22:53 +0100

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import BMI from "./pages/BMI";
-import Profile from "./pages/Profile";
-import NotFound from "./pages/NotFound";
+import {Router} from "./router/Router.tsx";
+
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bmi" element={<BMI />} />
-        <Route path="profile/:userId" element={<Profile />} />
-        <Route path="/404" element={<NotFound />} />
-      </Routes>
-    </Router>
+        <Router/>
   );
 }
 
