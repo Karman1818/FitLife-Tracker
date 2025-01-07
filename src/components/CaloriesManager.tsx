c1ea65e07b480d80107f7e4e44577ca9b1ec21cf
Karman <u38_markar_lbn@technischools.com>
2025-01-07 21:23:55 +0100

    Calories Manager 1 version

--- src/components/CaloriesManager.tsx
+++ src/components/CaloriesManager.tsx
@@ -1,23 +1,37 @@
+import { useCaloriesStore } from "../state/caloriesStore.ts";
+import AddProductForm from "./AddProductForm.tsx";
+import { useState } from "react";
 
-import {useCaloriesStore} from "../state/caloriesStore.ts";
 export const CaloriesManager = () => {
 
-    const Days = useCaloriesStore(state => state.days)
-    const addProductToMeal = useCaloriesStore(state => state.addProductToMeal)
+    const Days = useCaloriesStore(state => state.days);
+    const addProductToMeal = useCaloriesStore(state => state.addProductToMeal);
 
+    const [openMealForms, setOpenMealForms] = useState<{ [dayId: number]: { [mealId: number]: boolean } }>({});
 
-    return(
+    const Add = (dayId: number, mealId: number) => {
+        setOpenMealForms((prev) => ({
+            ...prev,
+            [dayId]: {
+                ...prev[dayId],
+                [mealId]: !prev[dayId]?.[mealId]
+            }
+        }));
+    };
+
+    return (
         <>
             {Days.map((day) => (
                 <div key={day.id}>
-                    {day.name}
+                    <h1>{day.name}</h1>
                     {day.meals.map((meal) => (
                         <span key={meal.id}>
                             {meal.name}
+                            <button onClick={() => Add(day.id, meal.id)}>Add new product to your meal </button>
+                            {openMealForms[day.id]?.[meal.id] && <AddProductForm mealId={meal.id} dayId={day.id} />}
                             {meal.products.map((product) => (
                                 <p key={product.id}>
                                     {product.name}
-
                                 </p>
                             ))}
                         </span>
@@ -25,5 +39,5 @@ export const CaloriesManager = () => {
                 </div>
             ))}
         </>
-    )
+    );
 }
