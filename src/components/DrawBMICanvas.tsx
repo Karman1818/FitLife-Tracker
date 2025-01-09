f69cc2549a5630c2b6e1c83042774ee0fa1b83fb
justPoosai <u50_oscsta_lbn@technischools.com>
2025-01-09 17:59:21 +0100

    BMI Fix

---
+++ src/components/DrawBMICanvas.tsx
@@ -0,0 +1,146 @@
+import React, { useEffect, useRef } from "react";
+
+interface DrawBMICanvasProps {
+  bmi: number | null;
+  currentAngle: number;
+  setCurrentAngle: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const DrawBMICanvas: React.FC<DrawBMICanvasProps> = ({
+  bmi,
+  currentAngle,
+  setCurrentAngle,
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const drawColoredDiagram = (ctx: CanvasRenderingContext2D) => {
+    const centerX = 200;
+    const centerY = 200;
+    const outerRadius = 158;
+    const innerRadius = 110;
+
+    const ranges = [
+      { color: "#FF5722", start: 0, end: 10 },
+      { color: "#FF9800", start: 10, end: 16 },
+      { color: "#FFE400", start: 16, end: 17 },
+      { color: "#D4E157", start: 17, end: 18.5 },
+      { color: "#4CAF50", start: 18.5, end: 25 },
+      { color: "#FF9800", start: 25, end: 30 },
+      { color: "#FF5722", start: 30, end: 35 },
+      { color: "#D20000", start: 35, end: 40 },
+      { color: "#900000", start: 40, end: 50 },
+    ];
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const anglePerBmi = Math.PI / 50;
+
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+
+    ranges.forEach((range) => {
+      const startAngle = anglePerBmi * range.start - Math.PI;
+      const endAngle = anglePerBmi * range.end - Math.PI;
+
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, outerRadius, startAngle, endAngle, false);
+      ctx.arc(centerX, centerY, innerRadius, endAngle, startAngle, true);
+      ctx.closePath();
+      ctx.fillStyle = range.color;
+      ctx.fill();
+    });
+
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, innerRadius, 0, Math.PI, true);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fill();
+  };
+
+  const drawArrowWithAnimation = (
+    ctx: CanvasRenderingContext2D,
+    angle: number
+  ) => {
+    const centerX = 200;
+    const centerY = 200;
+    const arrowLength = 145; // Skrócona długość strzałki, tak aby nachodziła na kolorowe pola
+    const arrowHeadLength = 20;
+
+    const startX = centerX;
+    const startY = centerY;
+
+    // Końcowy punkt strzałki na zewnętrznej krawędzi
+    const endX = startX + arrowLength * Math.cos(angle);
+    const endY = startY + arrowLength * Math.sin(angle);
+
+    // Rysowanie grotów strzałki (odwrócone o 180 stopni)
+    const arrowAngle = Math.PI / 8;
+    const arrowHeadLeftX =
+      endX + arrowHeadLength * Math.cos(angle - arrowAngle);
+    const arrowHeadLeftY =
+      endY + arrowHeadLength * Math.sin(angle - arrowAngle);
+    const arrowHeadRightX =
+      endX + arrowHeadLength * Math.cos(angle + arrowAngle);
+    const arrowHeadRightY =
+      endY + arrowHeadLength * Math.sin(angle + arrowAngle);
+
+    ctx.beginPath();
+    ctx.moveTo(startX, startY); // Zaczynamy w środku diagramu
+    ctx.lineTo(endX, endY); // Rysujemy trzon strzałki
+    ctx.moveTo(endX, endY);
+    ctx.lineTo(arrowHeadLeftX, arrowHeadLeftY);
+    ctx.lineTo(arrowHeadRightX, arrowHeadRightY);
+    ctx.closePath();
+    ctx.fillStyle = "#666";
+    ctx.fill();
+  };
+
+  const animateArrow = (
+    ctx: CanvasRenderingContext2D,
+    startAngle: number,
+    targetAngle: number
+  ) => {
+    const step = (targetAngle - startAngle) / 50;
+    let current = startAngle;
+
+    const interval = setInterval(() => {
+      current += step;
+      if (
+        (step > 0 && current >= targetAngle) ||
+        (step < 0 && current <= targetAngle)
+      ) {
+        current = targetAngle;
+        clearInterval(interval);
+      }
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      drawColoredDiagram(ctx);
+      drawArrowWithAnimation(ctx, current);
+      setCurrentAngle(current);
+    }, 20);
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        drawColoredDiagram(ctx);
+        if (bmi !== null) {
+          const anglePerBmi = Math.PI / 50;
+          const targetAngle =
+            anglePerBmi * Math.min(50, Math.max(0, bmi)) - Math.PI;
+          animateArrow(ctx, currentAngle, targetAngle);
+        }
+      }
+    }
+  }, [bmi]);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <canvas ref={canvasRef} className="bmi-chart" width="400" height="200" />
+      {/* <p style={{ marginTop: "20px", fontSize: "18px" }}>
+        Kategoria BMI: {getBMICategory()}
+      </p> */}
+    </div>
+  );
+};
+
+export default DrawBMICanvas;
--- src/pages/BMI.tsx
+++ src/pages/BMI.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import HeaderNav from "../components/HeaderNav";
 import FooterNav from "../components/FooterNav";
 import "../styles/BMI.css";
+import DrawBMICanvas from "../components/DrawBMICanvas";
 
-const BMIChart: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+const BMIChart = () => {
+  // const canvasRef = useRef<HTMLCanvasElement>(null);
   const [bmi, setBmi] = useState<number | null>(null);
   const [currentAngle, setCurrentAngle] = useState<number>(-Math.PI); // Start od lewej strony (-90°)
   const [formData, setFormData] = useState({ weight: "", height: "" });
@@ -12,145 +13,6 @@ const BMIChart: React.FC = () => {
   const calculateBmi = (weight: number, height: number): number => {
     return weight / Math.pow(height / 100, 2); // Wzrost w cm
   };
-
-  const drawColoredDiagram = (ctx: CanvasRenderingContext2D) => {
-    const centerX = 200;
-    const centerY = 200;
-    const outerRadius = 158; // Zwiększenie zewnętrznego promienia o 8 (158 zamiast 150)
-    const innerRadius = 110; // Wewnętrzny promień pozostaje bez zmian
-
-    // Przedziały BMI i kolory
-    const ranges = [
-      { color: "#FFE400", start: 0, end: 16 }, // Mocna niedowaga
-      { color: "#D4E157", start: 16, end: 18.4 }, // Lekka niedowaga
-      { color: "#4CAF50", start: 18.5, end: 24.9 }, // Prawidłowa waga
-      { color: "#FF9800", start: 25, end: 29.9 }, // Nadwaga
-      { color: "#FF5722", start: 30, end: 34.9 }, // Otyłość I
-      { color: "#D20000", start: 35, end: 39.9 }, // Otyłość II
-      { color: "#900000", start: 40, end: 50 }, // Otyłość III
-    ];
-
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    const anglePerBmi = Math.PI / 50; // Skala BMI 0-50 na półkole (180°)
-
-    // Ustawienie lini, by nie były widoczne przerwy
-    ctx.lineJoin = "round";
-    ctx.lineCap = "round";
-
-    ranges.forEach((range) => {
-      const startAngle = anglePerBmi * range.start - Math.PI; // Obrót o -90°
-      const endAngle = anglePerBmi * range.end - Math.PI;
-
-      ctx.beginPath();
-      ctx.arc(centerX, centerY, outerRadius, startAngle, endAngle, false);
-      ctx.arc(centerX, centerY, innerRadius, endAngle, startAngle, true);
-      ctx.closePath();
-      ctx.fillStyle = range.color;
-      ctx.fill();
-    });
-
-    // Biały środek półkola
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, innerRadius, 0, Math.PI, true);
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fill();
-  };
-
-  const drawArrowWithAnimation = (
-    ctx: CanvasRenderingContext2D,
-    angle: number
-  ) => {
-    const centerX = 200; // Środek półkola
-    const centerY = 200;
-    const arrowWidth = 2;
-    const arrowLength = 100; // Długość samej linii strzałki (wydłużona linia)
-
-    const arrowHeadLength = 20; // Stała długość trójkąta (końcówka strzałki)
-
-    // Początek strzałki - na dole białego półkola
-    const startX = centerX;
-    const startY = centerY; // Punkt początkowy na dole białego półkola
-
-    // Obliczamy współrzędne końca strzałki, która wychodzi na zewnątrz półkola
-    const endX = startX + arrowLength * Math.cos(angle);
-    const endY = startY + arrowLength * Math.sin(angle);
-
-    // Rysowanie linii strzałki
-    ctx.beginPath();
-    ctx.moveTo(startX, startY); // Początek strzałki
-    ctx.lineTo(endX, endY); // Koniec strzałki
-    ctx.lineWidth = arrowWidth;
-    ctx.strokeStyle = "#666";
-    ctx.stroke();
-
-    // Rysowanie końcówki strzałki (trójkąt)
-    const arrowAngle = Math.PI / 8; // Kąt końcówki strzałki
-    const arrowHeadLeftX =
-      endX - arrowHeadLength * Math.cos(angle - arrowAngle);
-    const arrowHeadLeftY =
-      endY - arrowHeadLength * Math.sin(angle - arrowAngle);
-    const arrowHeadRightX =
-      endX - arrowHeadLength * Math.cos(angle + arrowAngle);
-    const arrowHeadRightY =
-      endY - arrowHeadLength * Math.sin(angle + arrowAngle);
-
-    // Rysowanie trójkąta (końcówka strzałki)
-    ctx.beginPath();
-    ctx.moveTo(endX, endY);
-    ctx.lineTo(arrowHeadLeftX, arrowHeadLeftY);
-    ctx.lineTo(arrowHeadRightX, arrowHeadRightY);
-    ctx.closePath();
-    ctx.fillStyle = "#666";
-    ctx.fill();
-
-    // Dodanie kółka na przeciwnym końcu strzałki (anchor)
-    const anchorRadius = 5; // Promień kółka
-    ctx.beginPath();
-    ctx.arc(startX, startY, anchorRadius, 0, 2 * Math.PI); // Kółko na końcu strzałki
-    ctx.fillStyle = "#666"; // Kolor kółka
-    ctx.fill();
-  };
-
-  const animateArrow = (
-    ctx: CanvasRenderingContext2D,
-    startAngle: number,
-    targetAngle: number
-  ) => {
-    const step = (targetAngle - startAngle) / 50; // 50 kroków do osiągnięcia celu
-    let current = startAngle;
-
-    const interval = setInterval(() => {
-      current += step;
-      if (
-        (step > 0 && current >= targetAngle) ||
-        (step < 0 && current <= targetAngle)
-      ) {
-        current = targetAngle;
-        clearInterval(interval);
-      }
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      drawColoredDiagram(ctx);
-      drawArrowWithAnimation(ctx, current);
-      setCurrentAngle(current); // Aktualizacja bieżącego kąta
-    }, 20); // Animacja co 20 ms
-  };
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      const ctx = canvas.getContext("2d");
-      if (ctx) {
-        drawColoredDiagram(ctx);
-        if (bmi !== null) {
-          const anglePerBmi = Math.PI / 50; // Skala BMI 0-50 na półkole
-          const targetAngle =
-            anglePerBmi * Math.min(50, Math.max(0, bmi)) - Math.PI; // Obrót o -90°
-          animateArrow(ctx, currentAngle, targetAngle);
-        }
-      }
-    }
-  }, [bmi]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -167,41 +29,121 @@ const BMIChart: React.FC = () => {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
+  const weightRange = document.querySelector(
+    "#weight-range"
+  ) as HTMLInputElement | null;
+  const weightValue = document.querySelector(".weight-value");
+
+  const heightRange = document.querySelector(
+    "#height-range"
+  ) as HTMLInputElement | null;
+  const heightValue = document.querySelector(".height-value");
+
+  if (weightRange && weightValue) {
+    weightRange.addEventListener("input", (event) => {
+      const target = event.target as HTMLInputElement;
+      const tempSliderValueW = target.value;
+      weightValue.textContent = tempSliderValueW;
+
+      const progress =
+        (parseFloat(tempSliderValueW) / parseFloat(weightRange.max)) * 100;
+
+      weightRange.style.background = `linear-gradient(to right, #f50 ${progress}%, #ccc ${progress}%)`;
+    });
+  }
+
+  if (heightRange && heightValue) {
+    heightRange.addEventListener("input", (event) => {
+      const target = event.target as HTMLInputElement;
+      const tempSliderValueH = target.value;
+      heightValue.textContent = tempSliderValueH;
+
+      const progress =
+        (parseFloat(tempSliderValueH) / parseFloat(heightRange.max)) * 100;
+
+      heightRange.style.background = `linear-gradient(to right, #f50 ${progress}%, #ccc ${progress}%)`;
+    });
+  }
+
+  const getBMICategory = (): string => {
+    if (bmi === null) return "Nieznana kategoria"; // jeśli bmi jest null
+
+    if (bmi < 16) {
+      return "Anoreksja";
+    } else if (bmi >= 17 && bmi <= 18.4) {
+      return "Niedowaga";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      return "Waga normalna";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      return "Nadwaga";
+    } else if (bmi >= 30 && bmi <= 34.9) {
+      return "Otyłość I stopnia";
+    } else if (bmi >= 35 && bmi <= 39.9) {
+      return "Otyłość II stopnia";
+    } else {
+      return "Otyłość III stopnia";
+    }
+  };
+
   return (
     <div>
       <div className="bmi-container">
+      <h1 className="header">Calculate your BMI</h1>
         <form onSubmit={handleSubmit}>
           <label>
             Weight (kg):
             <input
-              type="number"
+              className="rangeslider"
+              id="weight-range"
+              type="range"
+              min="0"
+              max="200"
               name="weight"
-              value={formData.weight}
+              value={formData.weight || 0}
               onChange={handleInputChange}
             />
+            <div className="weight-value">0</div>
           </label>
-          <br />
           <label>
             Height (cm):
             <input
-              type="number"
+              className="rangeslider"
+              id="height-range"
+              type="range"
+              min="0"
+              max="250"
               name="height"
-              value={formData.height}
+              value={formData.height || 0}
               onChange={handleInputChange}
             />
+            <div className="height-value">0</div>
           </label>
           <br />
-          <button type="submit">Calculate BMI</button>
+          <button className="calculate" type="submit">
+            Calculate BMI
+          </button>
         </form>
-        <canvas
-          ref={canvasRef}
-          className="half-circle"
-          width="400"
-          height="200"
-        />
-        {bmi !== null && (
-          <p className="bmi-result">Your BMI: {bmi.toFixed(2)}</p>
-        )}
+        <div className="bmi-chart-result">
+          {bmi !== null && (
+            <div>
+              <h1 className="h1-bmi-result">
+                BMI:
+                <span>{bmi.toFixed(2)}</span>
+              </h1>
+              <p
+                className="bmi-category"
+                // style={{ marginTop: "20px", fontSize: "18px" }}
+              >
+                Kategoria BMI: {getBMICategory()}
+              </p>
+            </div>
+          )}
+          <DrawBMICanvas
+            bmi={bmi}
+            currentAngle={currentAngle}
+            setCurrentAngle={setCurrentAngle}
+          />
+        </div>
       </div>
       <HeaderNav />
       <FooterNav />
