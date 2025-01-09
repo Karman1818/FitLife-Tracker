ffb316da28cc1e105be2aeb177caaca9b1d266ed
Averi <git@averi.me>
2025-01-09 06:23:33 -0500

    nuke

--- src/pages/Meals.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const Meals = () => {
-    const { userId } = useParams<{ userId: string }>();
-    return <h1>Sugestie posiłkow dla usera {userId}</h1>;
-};
-
-export default Meals;
