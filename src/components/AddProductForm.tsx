de6a21c644c461d843ceed5a20435bb47f291668
Karman <u38_markar_lbn@technischools.com>
2025-01-12 13:57:35 +0100

    adding product to meal with AddProductForm fix

--- src/components/AddProductForm.tsx
+++ src/components/AddProductForm.tsx
@@ -17,7 +17,7 @@ const AddProductForm = ({mealId, dayId}) => {
     fat:"",
   });
 
-  const addProductToMeal = useCaloriesStore((state) => state.addProductToMeal)
+  const addProductToMeal = useCaloriesStore((state) => state.addProductToMeal);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -42,18 +42,50 @@ const AddProductForm = ({mealId, dayId}) => {
       carbohydrates: formData.carbohydrates,
       fat: formData.fat,
     };
-    addProductToMeal(dayId,mealId,product);
+    addProductToMeal(dayId, mealId, product);
   };
 
   const [options, setOptions] = useState<ProductOption[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingProducts, setIsLoadingProducts] = useState(false);
+  const [isLoadingNutrients, setIsLoadingNutrients] = useState(false);
+
+  const fetchProductNutrients = async (productId: string) => {
+    const URL = `https://world.openfoodfacts.org/api/v0/product/${productId}.json`;
+
+    try {
+      setIsLoadingNutrients(true);
+      const response = await fetch(URL);
+      const data = await response.json();
+      const productData = data.product;
+
+      const nutriments = {
+        calories: productData.nutriments?.energy || 'Unknown',
+        protein: productData.nutriments?.proteins_100g || 'Unknown',
+        carbohydrates: productData.nutriments?.carbohydrates_100g || 'Unknown',
+        fat: productData.nutriments?.fat_100g || 'Unknown',
+      };
+
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        calories: nutriments.calories,
+        protein: nutriments.protein,
+        carbohydrates: nutriments.carbohydrates,
+        fat: nutriments.fat,
+      }));
+
+    } catch (error) {
+      console.error('Fetching nutriments error', error);
+    } finally {
+      setIsLoadingNutrients(false);
+    }
+  };
 
   const fetchProducts = async (inputValue: string) => {
     if (!inputValue) return [];
     const URL = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${encodeURIComponent(inputValue)}&search_simple=1&json=1`;
 
     try {
-      setIsLoading(true);
+      setIsLoadingProducts(true);
       const response = await fetch(URL);
       const data = await response.json();
       const products = data.products.map((product: any) => ({
@@ -64,7 +96,7 @@ const AddProductForm = ({mealId, dayId}) => {
     } catch (error) {
       console.error('Error while downloading products', error);
     } finally {
-      setIsLoading(false);
+      setIsLoadingProducts(false);
     }
   };
 
@@ -72,11 +104,15 @@ const AddProductForm = ({mealId, dayId}) => {
     fetchProducts(newValue);
   };
 
-  const handleSelectChange = (selectedOption: ProductOption | null) => {
-    setFormData({
-      ...formData,
-      name: selectedOption?.label || '',
-    });
+
+  const handleSelectChange = async (selectedOption: ProductOption | null) => {
+    if (selectedOption) {
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        name: selectedOption.label,
+      }));
+      await fetchProductNutrients(selectedOption.value);
+    }
   };
 
   return (
@@ -87,7 +123,7 @@ const AddProductForm = ({mealId, dayId}) => {
             <Select
                 options={options}
                 onInputChange={handleInputChange}
-                isLoading={isLoading}
+                isLoading={isLoadingProducts}
                 placeholder="Enter the product name..."
                 noOptionsMessage={() => 'No results found'}
                 value={options.find(option => option.label === formData.name) || null}
@@ -105,6 +141,26 @@ const AddProductForm = ({mealId, dayId}) => {
                 onChange={handleChange}
                 required
             />
+          </label>
+        </div>
+        <div>
+          <label>
+            Calories: {formData.calories}
+          </label>
+        </div>
+        <div>
+          <label>
+            Protein: {formData.protein}
+          </label>
+        </div>
+        <div>
+          <label>
+            Carbohydrates: {formData.carbohydrates}
+          </label>
+        </div>
+        <div>
+          <label>
+            Fats: {formData.fat}
           </label>
         </div>
         <button type="submit">Add product</button>
