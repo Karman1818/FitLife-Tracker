8c06c1fa1b8e1308715d53e1512b70ceabe79fae
Karman <u38_markar_lbn@technischools.com>
2025-01-12 18:01:19 +0100

    literówka fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,7 @@ export const Navigation = () => {
     return(
             <Link>
                 <Link to={"/"}>Home </Link>
-                <Link to={"/Calories"}>Caloreis </Link>
+                <Link to={"/Calories"}>Calories </Link>
                 <Link to={"/BMI"}>Bmi </Link>
                 <Link to={"/Meals"}>Meals </Link>
                 <Link to={"/Profile"}>Profile </Link>
