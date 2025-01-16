c0556bf318331b7a1fd116e3eb6220df40a0c53f
Averi <git@averi.me>
2025-01-16 20:17:06 +0100

    Removed problematic W.I.P. Stuff

--- auth.ts
+++ auth.ts
@@ -1,16 +1,7 @@
 import NextAuth from "next-auth";
-import Resend from "@auth/core/providers/resend";
-import { D1Adapter } from "@auth/d1-adapter";
-import env from "@/lib/env";
-import { Provider } from "@auth/core/providers";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: D1Adapter(env.db),
-  providers: [
-    Resend({
-      from: env.email_from,
-    })
-  ],
+  providers: [],
   theme: {
     colorScheme: "dark",
     brandColor: "#9900ff",
