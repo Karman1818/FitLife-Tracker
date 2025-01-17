47c16054a64465cf2c36fdcd724fb68c94965cba
Averi <git@averi.me>
2025-01-17 06:30:20 -0500

    Removed all the cloudflare stuff

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
   images: {
@@ -9,9 +8,5 @@ const nextConfig: NextConfig = {
     }],
   },
 };
-
-if(process.env.NODE_ENV === "development") {
-  setupDevPlatform();
-}
 
 export default nextConfig;
