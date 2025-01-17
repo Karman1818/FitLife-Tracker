1465938678a34fa26018759ed150470f2c46cb58
Averi <git@averi.me>
2025-01-17 07:48:06 -0500

    Basic prisma setup

--- auth.ts
+++ auth.ts
@@ -1,10 +1,18 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import Resend from "@auth/core/providers/resend";
+import { env } from "@/lib/env";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
+    Resend({
+      from: env.EMAIL_FROM,
+    }),
   ],
   theme: {
     colorScheme: "dark",
     brandColor: "#9900ff",
-  }
+  },
 });
